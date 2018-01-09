ff9260d416dd6ecf94351054139dc84c48c48cc9
Francisco Bernal Yescas <fbernaly@gmail.com>
2018-01-09 00:48:10 -0800

    XYZ

---
+++ XPLO/Model/Renderer/XYZ.swift
@@ -0,0 +1,16 @@
+//
+//  XYZ.swift
+//  XPLO
+//
+//  Created by Francisco Bernal Yescas on 1/9/18.
+//  Copyright © 2018 Sean Fredrick, LLC. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct XYZ {
+  var x: Float
+  var y: Float
+  var z: Float
+}
