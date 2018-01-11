faae77049c66249241f8ad59ca6c04b6ea79395c
Francisco Bernal Yescas <fbernaly@gmail.com>
2018-01-10 18:03:23 -0800

    files

---
+++ XPLO/AppDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.swift
+//  XPLO
+//
+//  Created by Francisco Bernal Yescas on 12/5/17.
+//  Copyright © 2017 Sean Fredrick, LLC. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+  
+  var window: UIWindow?
+  
+}
